057685451d3ec7f5dffb819fbe7feecbf3e88cf8
Elxafil <daniel.alexander@randamonium.com>
2019-09-25 07:34:11 +1200

    Cleanup Admin

--- modules/Admin.py
+++ modules/Admin.py
@@ -2,7 +2,10 @@ import discord
 from discord.ext import commands
 
 class Admin:
-    def __init__(self, bot): self.bot = bot
+    def __init__(self, bot):
+    
+    # Define Variables
+    self.bot = bot
     
     admins = [
         536507052558909461, # Network Owner
